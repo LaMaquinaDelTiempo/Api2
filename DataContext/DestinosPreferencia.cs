7cd9bd55a621b0d9625ea8a66a730df7f1a10c8b
DiegoHernandez <diego0930@gmail.com>
2025-03-16 12:47:11 -0500

    Agregar archivos de proyecto.

---
+++ DataContext/DestinosPreferencia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.DataContext;
+
+public partial class DestinosPreferencia
+{
+    public long PreferenciasId { get; set; }
+
+    public long DestinosId { get; set; }
+
+    public virtual Destino Destinos { get; set; } = null!;
+
+    public virtual Preferencia Preferencias { get; set; } = null!;
+}
---
+++ DataContext/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.DataContext;
+
+public partial class Usuario
+{
+    public long Id { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Password { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<PreferenciaUsuario> PreferenciaUsuarios { get; set; } = new List<PreferenciaUsuario>();
+}

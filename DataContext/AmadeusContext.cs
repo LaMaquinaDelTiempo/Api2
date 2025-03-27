342dc009fd706bd08f5e2775e728ce6869f7090d
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-03-26 19:44:02 -0500

    Establishing connection with the remote database

--- DataContext/AmadeusContext.cs
+++ DataContext/AmadeusContext.cs
@@ -30,7 +30,7 @@ public partial class AmadeusContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=amadeus;Username=postgres;Password=1234");
+        => optionsBuilder.UseNpgsql("Host=database-1.cvsvuhbh6kob.us-east-1.rds.amazonaws.com;Database=nullpointers;Username=postgres;Password=M4_bPT-Hwq8Plf>w>jjm40k3!dt(");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

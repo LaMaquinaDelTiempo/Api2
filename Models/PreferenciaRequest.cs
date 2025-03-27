a2f1fc8bd057122bee3b8b085a35ba8bf99e3a93
DiegoHernandez <diego0930@gmail.com>
2025-03-27 18:22:41 -0500

    Creacion de modelo para Request

---
+++ Models/PreferenciaRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Models
+public class PreferenciaRequest
+{
+    public string Email { get; set; }           // "probandsfds13@gmail.com"
+    public string Entorno { get; set; }         // "CIUDAD"
+    public string Clima { get; set; }           // "CALUROSO"
+    public string Actividad { get; set; }       // "DEPORTES_Y_AVENTURAS"
+    public string Alojamiento { get; set; }     // "HOTEL_LUJO"
+    public string TiempoViaje { get; set; }     // "MENOR_UNA_SEMANA"
+    public string RangoEdad { get; set; }       // "MENOR_QUE_TREINTA"
+}
+

b6d7a1cb4a8455e5d17c0da6ee7ebede7ad8635d
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-04-03 13:13:19 -0500

    -Eliminar cadena de conexión hardcodeada del contexto de base de datos -Removido string de conexión expuesto en AmadeusContext -Eliminado warning de seguridad sobre información sensible -La conexión ahora se usa exclusivamente desde appsettings.json -Mejorada seguridad al evitar credenciales en código fuente

--- DataContext/AmadeusContext.cs
+++ DataContext/AmadeusContext.cs
@@ -29,8 +29,7 @@ namespace Api.DataContext
         public virtual DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-            => optionsBuilder.UseNpgsql("Host=database-1.cvsvuhbh6kob.us-east-1.rds.amazonaws.com;Database=nullpointers;Username=postgres;Password=M4_bPT-Hwq8Plf>w>jjm40k3!dt(");
+        { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
